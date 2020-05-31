8df8edcf9394a276ca3187901a1c7e66d3fc3d42
Reinaldo Salla <reinaldosalla95@gmail.com>
2020-05-31 17:02:25 -0300

    More on promises

--- promises-and-async-await.js
+++ promises-and-async-await.js
@@ -1,39 +1,49 @@
-const simulateApi = () => 
-	Math.random() > 0.5 ? true : false;
+/*
+1-multiple ones, one depending on the other
+3-Promise.all
+4-Promise.allSettled
+*/
+
+const simulateApi = () => (
+	Math.random() > 0.5 ? true : false	
+);
 
 const learnJavaScript = () => {
-	const learned = simulateApi();
+	const learned = true
 	return new Promise((resolve, reject) => 
 		learned 
-			? resolve("an expert in JavaScript") 
-			: reject("an novice in JavaScript")
+			? resolve("I'm an expert in JavaScript") 
+			: reject("I'm an novice in JavaScript")
 	);
 };
 
-const learnNode = () => {
+const learnNode = (jsStatus) => {
 	const learned = true;
 	return new Promise((resolve, reject) =>
 		learned 
-			?	resolve("an expert in Node")
-			: resolve("an novice in Node")
+			?	resolve(`${jsStatus}\nI'm also an expert in Node`)
+			: reject(`${jsStatus}\nI'm also an novice in Node`)
 	);
 };
 
-/*
+const learnReact = (nodeStatus) => {
+	const learned = true;
+	return new Promise((resolve, reject) => 
+		learned
+			? resolve(`${nodeStatus}\nI'm also an expert in React`)
+			: reject(`${nodeStatus}\nI'm also an novice in React`)
+	);
+};
+
 const getStatus = () => {
 	let status;
 	learnJavaScript()
-		.then(result =>	{
-			status = result
-		})
-		.catch(err => {
-			status = err
-		})
-		.finally(() => 
-			console.log(`I am ${status}`)
-		);
+		.then(jsStatus => learnNode(jsStatus))
+		.then(nodeStatus => learnReact(nodeStatus))
+		.then(reactStatus => status = reactStatus)
+		.catch(err => console.error(err))
+		.finally(() => console.log(status));
 };
-*/
 
 /*
 const getStatus = async () => {
